1e0333b09c10a6049d8c2c2c9102e35249011654
Payal Dayma <daymapayal1819@gmail.com>
2024-05-23 13:55:56 +0530

    My First commit

---
+++ src/com/payal/example/Demo.java
@@ -0,0 +1,21 @@
+package com.payal.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+	public void m1()
+	{
+		List<String> list = new ArrayList<String>();
+		list.add("core java");
+		list.add("collection framework");
+		list.add("Database");
+		list.add("git");
+		System.out.println(list);
+	}
+	public static void main(String[] args)
+	{
+		Demo demo = new Demo();
+		demo.m1();
+	}
+}
